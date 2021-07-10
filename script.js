582082686f31c649ba601418f509aeb2a2ad0e7d
Piyush1607 <2019301@iiitdmj.ac.in>
2021-07-10 12:11:25 +0530

    Adding Smooth Scroll

--- script.js
+++ script.js
@@ -29,3 +29,12 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// SMOOTH --- SCROLLING
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 =document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click',function(e){
+  // MODERN SMOOTH SCROLL
+  section1.scrollIntoView({behavior:'smooth'})
+})
